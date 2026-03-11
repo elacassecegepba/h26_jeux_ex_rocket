8a052eb3e3a8b6cf804a4ebdc1090e56131c304e
elacassecegepba <114033700+elacassecegepba@users.noreply.github.com>
2026-03-11 14:10:57 -0400

    Tourner

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,8 +6,12 @@ using UnityEngine.InputSystem;
 public class Rocket : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+
     private bool _isThrusting;
-    [SerializeField] private float _thrustForce = 42f;
+    [SerializeField] private float _thrustForce = 21f;
+
+    private Vector2 _inputVector;
+    [SerializeField] private float _rotationSpeed = 21f; 
 
     void Start()
     {
@@ -18,9 +22,15 @@ public class Rocket : MonoBehaviour
         if (_isThrusting) {
             _rigidbody.AddRelativeForce(Vector3.up * _thrustForce, ForceMode.Acceleration);
         }
+
+        _rigidbody.AddRelativeTorque(Vector3.forward * -_inputVector.x * _rotationSpeed, ForceMode.Acceleration);
     }
 
     void OnJump(InputValue value) {
         _isThrusting = value.isPressed;
+    }
+
+    void OnMove(InputValue value) {
+        _inputVector = value.Get<Vector2>();
     }
 }
